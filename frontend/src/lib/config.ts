a3adfbe3d0d602b573f1d79b4aab8233b5b30f25
Mukhtar Axmed <143383104+MoDev40@users.noreply.github.com>
2024-02-13 23:52:44 +0300

    Update config.ts
    Internet Api

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -5,3 +5,4 @@ export const storageBucket = import.meta.env.VITE_STORAGE_BUCKET as string
 export const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID as string
 export const appId = import.meta.env.VITE_APP_ID as string
 export const measurementId = import.meta.env.VITE_MEASUREMENT_ID as string
+export const backendApi = import.meta.env.VITE_BACKEND_API as string
