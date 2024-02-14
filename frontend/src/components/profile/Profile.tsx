527f05b9b44d2fc29b048ae5c4fcb960094f6a20
Mukhtar Axmed <143383104+MoDev40@users.noreply.github.com>
2024-02-14 06:33:33 +0300

    Update Profile.tsx

--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useAuth } from "@/hooks/AuthUser"
